bae40e7e778cb2ef62e51f890f3a35182b1c7b69
Alexzhxy <zxyperfect@gmail.com>
2017-04-20 15:53:51 -0700

    replace parse-image by sharp to resolve aws module issue

--- cloud/delegates/image_manager.js
+++ cloud/delegates/image_manager.js
@@ -2,111 +2,122 @@ var _ = require('underscore');
 var image_assembler = require('../assemblers/image');
 var error_handler = require('../error_handler');
 var UserActivity = Parse.Object.extend('UserActivity');
-var ImageDatabase = Parse.Object.extend('Image');
+var Image = Parse.Object.extend('Image');
+var sharp = require('sharp');
 
-// exports.upload = function(req, res){
-// 	var base64Code = req.body["base64_code"];
-// 	var file = new Parse.File("profile.jpeg", { base64: base64Code });
+'use strict'
 
-// 	var image = new ImageDatabase();
-// 	image.set("original", file);
+const ORIGINAL_SIZE = 800;
+const THUMBNAIL_SIZE = 100;
 
-// 	image.save().then(function(_image) {
-// 		var imageRes = {};
-// 		imageRes["result"] = image_assembler.assemble(_image);
-// 		res.status(200).json(imageRes);
-// 	}, function(error) {
-// 		error_handler.handle(error, {}, res);
-// 	});
-// }
+exports.uploadImage = function (req, res) {
+  var restaurantId = req.body["restaurant_id"];
+  var type = req.body["type"];
+  var base64Code = req.body["base64_code"];
+  var clientEventId = req.body["event_id"];
+  var user = req.user;
 
-exports.uploadImage = function(req, res) {
-	var restaurantId = req.body["restaurant_id"];
-	var type = req.body["type"];
-	var base64Code = req.body["base64_code"];
-	var clientEventId = req.body["event_id"];
-	var user = req.user; 
+  var p1 = createResizedImage(base64Code, ORIGINAL_SIZE);
+  var p2 = createResizedImage(base64Code, THUMBNAIL_SIZE);
 
-	var file = new Parse.File(type + ".jpeg", { base64: base64Code });
+  Parse.Promise.when(p1, p2).then(function (originalBase64Code, thumbnailBase64Code) {
+    var originalImage = new Parse.File(type + ".jpeg", {base64: originalBase64Code});
+    var thumbnailImage = new Parse.File(type + ".jpeg", {base64: thumbnailBase64Code});
+    var newImage = new Image();
+    newImage.set("original", originalImage);
+    newImage.set("thumbnail", thumbnailImage);
+    newImage.set("type", type);
+    if (restaurantId != undefined) {
+      var restaurant = {
+        __type: "Pointer",
+        className: "Restaurant",
+        objectId: restaurantId
+      };
+      newImage.set("restaurant", restaurant);
+    }
 
-	var newImage = new ImageDatabase();
-	newImage.set("original", file);
-	newImage.set("type", type);
-	if (restaurantId != undefined) {
-		var restaurant = {
-	        __type: "Pointer",
-	        className: "Restaurant",
-	        objectId: restaurantId
-		};
-		newImage.set("restaurant", restaurant);
-	}  
-	
-	newImage.save().then(function(newImage) {
-		if (user != undefined) {
-			createUserActivity(clientEventId, user, newImage.id, restaurantId);
-			var userPoints = config['upload_image']['user_points'];
-			user.increment('points', userPoints);
-			user.save();
-		}
-		var imageRes = image_assembler.assemble(newImage);
-		var response = {};
-		response['result'] = imageRes;
-		res.status(200).json(response);
-	}, function(error) {
-		error_handler.handle(error, {}, res);
-	});
+    newImage.save().then(function (newImage) {
+      if (user != undefined) {
+        createUserActivity(clientEventId, user, newImage.id, restaurantId);
+        var userPoints = config['upload_image']['user_points'];
+        user.increment('points', userPoints);
+        user.save();
+      }
+      var imageRes = image_assembler.assemble(newImage);
+      var response = {};
+      response['result'] = imageRes;
+      res.status(200).json(response);
+    }, function (error) {
+      error_handler.handle(error, {}, res);
+    });
+  });
+}
+
+exports.findAllByRestaurantId = function (req, res) {
+  var restaurantId = req.query.restaurantId;
+  var restaurant = {
+    __type: "Pointer",
+    className: "Restaurant",
+    objectId: restaurantId
+  };
+  var query = new Parse.Query(Image);
+  query.equalTo('restaurant', restaurant);
+  query.find().then(function (images) {
+    var results = [];
+    if (images != undefined && images.length > 0) {
+      _.each(images, function (image) {
+        var result = image_assembler.assemble(image);
+        results.push(result);
+      });
+    }
+    var response = {};
+    response['results'] = results;
+    res.status(200).json(response)
+  }, function (error) {
+    error_handler.handle(error, {}, res);
+  });
+}
+
+function createResizedImage(base64Code, size) {
+  var promise = new Parse.Promise();
+
+  var buffer = new Buffer(base64Code, 'base64');
+  sharp(buffer)
+    .resize(size, size)
+    .max()
+    .toFormat('jpeg')
+    .toBuffer()
+    .then(data => {
+      promise.resolve(data.toString('base64'));
+    });
+
+  return promise;
 }
 
 function createUserActivity(eventId, user, imageId, restaurantId) {
-	var query = new Parse.Query(UserActivity);
-	query.equalTo('event_id', eventId);
-	query.find().then(function(activities){
-		var activity;
-		if (activities != undefined && activities.length > 0) {
-			activity = activities[0];
-		} else {
-			activity = new UserActivity();
-		}
-		activity.set('user', user);
-		activity.set('event_id', eventId);
-		activity.set('type', 'upload_image');
-		activity.add('photos', imageId);
-		if (activity.get('restaurant') == undefined) {
-			var restaurant = {
-		        __type: "Pointer",
-		        className: "Restaurant",
-		        objectId: restaurantId
-		    };
-		    activity.set('restaurant', restaurant);
-		}	
-		activity.save();
-	}, function(){
+  var query = new Parse.Query(UserActivity);
+  query.equalTo('event_id', eventId);
+  query.find().then(function (activities) {
+    var activity;
+    if (activities != undefined && activities.length > 0) {
+      activity = activities[0];
+    } else {
+      activity = new UserActivity();
+    }
+    activity.set('user', user);
+    activity.set('event_id', eventId);
+    activity.set('type', 'upload_image');
+    activity.add('photos', imageId);
+    if (activity.get('restaurant') == undefined) {
+      var restaurant = {
+        __type: "Pointer",
+        className: "Restaurant",
+        objectId: restaurantId
+      };
+      activity.set('restaurant', restaurant);
+    }
+    activity.save();
+  }, function () {
 
-	});
-}
-
-
-exports.findAllByRestaurantId = function(req, res) {
-	var restaurantId = req.query.restaurantId;
-	var restaurant = {
-	        __type: "Pointer",
-	        className: "Restaurant",
-	        objectId: restaurantId
-	};
-	var query = new Parse.Query(ImageDatabase);
-	query.equalTo('restaurant', restaurant);
-	query.find().then(function(images) {
-		var results = [];
-		if (images != undefined && images.length > 0) {
-			_.each(images, function(image) {
-				var result = image_assembler.assemble(image);
-				results.push(result);
-			});
-		}
-		var response = {};
-		response['results'] = results;
-		res.status(200).json(response)
-	}, function(error) {
-		error_handler.handle(error, {}, res);
-	});
+  });
 }
