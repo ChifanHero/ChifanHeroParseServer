dbd83cc48d759baf5fb73c5d45ebe46a6484add8
family.syan <family.syan@gmail.com>
2017-10-02 23:31:42 -0700

    Fix homepage bugs

--- cloud/delegates/homepageManager.js
+++ cloud/delegates/homepageManager.js
@@ -4,6 +4,7 @@ const _ = require('underscore');
 const Restaurant = Parse.Object.extend('Restaurant');
 const restaurantAssembler = require('../assemblers/restaurant');
 const errorHandler = require('../errorHandler');
+const ratingUtil = require('../util/ratingUtil.js');
 
 exports.getHomePages = function (req, res) {
   console.log('CFH_GetHomePage');
@@ -19,18 +20,22 @@ exports.getHomePages = function (req, res) {
   const response = {};
   Parse.Promise.when(p1, p2, p3).then(function (nearest, recommended, hottest) {
     const homepageSections = [];
+    dedupe(hottest, recommended); // if a restaurant is already recommended, don't show it in
+                                  // the "hottest" section so that we will be able to show more restaurants
     let placement = 0;
-    if (recommended.length >= 3) {
+    if (recommended !== undefined && recommended.length >= 3) {
       homepageSections.push(assembleResults(recommended, "英雄推荐", placement, latitude, longitude));
       nearest = dedupe(nearest, recommended);
       hottest = dedupe(hottest, recommended.concat(nearest));
       placement++;
     }
-    if (hottest.length >= 3) {
+    if (hottest !== undefined && hottest.length >= 3) {
       homepageSections.push(assembleResults(hottest, "热门餐厅", placement, latitude, longitude));
       placement++;
     }
-    homepageSections.push(assembleResults(nearest, "离您最近", placement, latitude, longitude));
+    if (nearest !== undefined && nearest.length > 0) {
+      homepageSections.push(assembleResults(nearest, "离您最近", placement, latitude, longitude));
+    }
     response['homepagesections'] = homepageSections;
     res.status(200).json(response);
   }, function (error) {
@@ -38,6 +43,24 @@ exports.getHomePages = function (req, res) {
     errorHandler.handle(error, res);
   });
 };
+
+function dedupe(from, exclude) {
+  if (from != undefined && from.length > 0 && exclude != undefined && exclude.length > 0) {
+    const excludedIds = {};
+    _.each(exclude, function (excluded) {
+      excludedIds[excluded.id] = true;
+    });
+    const deduped = [];
+    _.each(from, function(restaurant) {
+      if (excludedIds[restaurant.id] !== true) {
+        deduped.push(restaurant);
+      }
+    });
+    return deduped;
+  } else {
+    return from;
+  }
+}
 
 // nearest
 function findNearestRestaurants(limit, latitude, longitude) {
@@ -89,21 +112,70 @@ function findHotestRestaurants(limit, latitude, longitude) {
   const promise = new Parse.Promise();
   const query = new Parse.Query(Restaurant);
   query.include('image');
-  if (limit !== undefined) {
-    query.limit(limit);
-  }
+  query.descending('google_rating'); // This is a hack. some restaurants doesn't have "rating" but we still want to show them.
+  query.limit(limit * 2); // limit = limit * 2 because we will do local sorting
   const userGeoPoint = new Parse.GeoPoint(latitude, longitude);
   query.withinMiles("coordinates", userGeoPoint, 30);
   query.notEqualTo('blacklisted', true);
   query.notEqualTo('on_hold', true);
   
   query.find().then(function (results) {
-    promise.resolve(results);
+    const sorted = sortByRating(results);
+    if (sorted !== undefined && sorted.length >= limit) {
+      promise.resolve(sorted.slice(0, limit));
+    } else {
+      promise.resolve(sorted);
+    }
   }, function (error) {
     const empty = [];
     promise.resolve(empty);
   });
   return promise;
+}
+
+function sortByRating(restaurants) {
+  if (restaurants !== undefined && restaurants.length > 0) {
+    const sorted = _.sortBy(restaurants, function(restaurant){
+      return calculateRatingForParseObject(restaurant);
+    }).reverse();
+    return sorted;
+  } else {
+    return [];
+  }
+}
+
+function calculateRatingForParseObject(restaurant) {
+  if (restaurant !== undefined) {
+      let score1 = 0;
+      let score2 = 0;
+      let score3 = 0;
+      let score4 = 0;
+      let score5 = 0;
+      if (restaurant.get('score_1') !== undefined) {
+        score1 = restaurant.get('score_1');
+      }
+      if (restaurant.get('score_2') !== undefined) {
+        score2 = restaurant.get('score_2');
+      }
+      if (restaurant.get('score_3') !== undefined) {
+        score3 = restaurant.get('score_3');
+      }
+      if (restaurant.get('score_4') !== undefined) {
+        score4 = restaurant.get('score_4');
+      }
+      if (restaurant.get('score_5') !== undefined) {
+        score5 = restaurant.get('score_5');
+      }
+      const userRatingCount = score1 + score2 + score3 + score4 + score5;
+      let userRating = 0;
+      if (userRatingCount !== 0) {
+        userRating = (score1 + score2 * 2 + score3 * 3 + score4 * 4 + score5 * 5) / userRatingCount;  
+      }
+      let totalRating = ratingUtil.mergeRating(userRating, userRatingCount, restaurant.get('google_rating'));
+      return parseFloat(totalRating.toFixed(1));
+  } else {
+    return -1.0; // return a negative number so that it will be demoted.
+  }
 }
 
 function assembleResults(restaurants, title, placement, latitude, longitude) {
